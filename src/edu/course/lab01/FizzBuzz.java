16a13c4b938e8008d404dcf6748501b2489d25a8
Alex Sinyakov <zaki31@inbox.ru>
2026-09-20 18:19:40 +0700

    1

--- src/edu/course/lab01/FizzBuzz.java
+++ src/edu/course/lab01/FizzBuzz.java
@@ -6,7 +6,7 @@ public final class FizzBuzz {
 
     public static void run() {
         for (int i = 1; i <= 500; i++) {
-            if (i % 3 == 0 && i % 7 == 0) {
+            if (i % 5 == 0 && i % 7 == 0) {
                 System.out.println("fizzbuzz");
             } else if (i % 5 == 0) {
                 System.out.println("fizz");
